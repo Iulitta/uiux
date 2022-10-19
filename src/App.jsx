1e737aa56407b045eadb87682975a058dd7a5f32
iulitta katanaeva <iulittakatanaeva@yandex.ru>
2022-10-19 23:36:09 +0300

    styles added

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,39 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import './styles/index.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  const handleIncrement = () => {
+    setCount((count) => count + 1);
+  }
+
+  const handleDecrement = () => {
+    setCount((count) => count - 1);
+  }
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="root">
+      <nav className={"up_tab"}>
+        <h2 className={"up_text"}>Катанаева Иулитта БВТ1901</h2>
+      </nav>
+      <main className={"main"}>
+        <div className={"button_list"}>
+          <button className={"button"}>Кнопка 1</button>
+          <button className={"button"}>Кнопка 2</button>
+          <button className={"button"}>Кнопка 3</button>
+          <button className={"button"}>Кнопка 4</button>
+          <button className={"button"}>Кнопка 5</button>
+        </div>
+        <div className={"counter_background"}>
+          <p className={"counter_text"}>Счётчик</p>
+            <div className={"button_container"}>
+              <p className={"counter_text"}>{count}</p>
+              <button className={"counter_button"} onClick={handleDecrement}>-</button>
+              <button className={"counter_button"} onClick={handleIncrement}>+</button>
+            </div>
+        </div>
+      </main>
     </div>
   )
 }
