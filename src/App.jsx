743ebb66aa4efc2e8a7100133cbb47ce1684db50
iulitta katanaeva <iulittakatanaeva@yandex.ru>
2022-10-26 20:41:44 +0300

    Счётчик готов

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,15 @@
-import { useState } from 'react'
-import './styles/index.css'
+import Navbar from './components/Navbar.jsx'
+import ButtonList from './components/ButtonList.jsx'
+import Counter from './components/Counter.jsx'
+import './components/styles/index.css'
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  const handleIncrement = () => {
-    setCount((count) => count + 1);
-  }
-
-  const handleDecrement = () => {
-    setCount((count) => count - 1);
-  }
-
   return (
     <div className="root">
-      <nav className={"up_tab"}>
-        <h2 className={"up_text"}>Катанаева Иулитта БВТ1901</h2>
-      </nav>
+      <Navbar/>
       <main className={"main"}>
-        <div className={"button_list"}>
-          <button className={"button"}>Кнопка 1</button>
-          <button className={"button"}>Кнопка 2</button>
-          <button className={"button"}>Кнопка 3</button>
-          <button className={"button"}>Кнопка 4</button>
-          <button className={"button"}>Кнопка 5</button>
-        </div>
-        <div className={"counter_background"}>
-          <p className={"counter_text"}>Счётчик</p>
-            <div className={"button_container"}>
-              <p className={"counter_text"}>{count}</p>
-              <button className={"counter_button"} onClick={handleDecrement}>-</button>
-              <button className={"counter_button"} onClick={handleIncrement}>+</button>
-            </div>
-        </div>
+        <ButtonList/>
+        <Counter/>
       </main>
     </div>
   )
---
+++ src/components/ButtonList.jsx
@@ -0,0 +1,15 @@
+import './styles/navbar.css'
+
+const ButtonList = () => {
+    return (
+        <div className={"button_list"}>
+          <button className={"button"}>Кнопка 1</button>
+          <button className={"button"}>Кнопка 2</button>
+          <button className={"button"}>Кнопка 3</button>
+          <button className={"button"}>Кнопка 4</button>
+          <button className={"button"}>Кнопка 5</button>
+        </div>
+    )
+}
+
+export default ButtonList;
